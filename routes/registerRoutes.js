a85a200dab0b039ff435b1ccf84335d0da768729
stevenkeezer <stevengkeezer@gmail.com>
2021-02-02 14:38:48 -0800

    removed comments

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -5,12 +5,7 @@ const bodyParser = require("body-parser")
 const bcrypt = require('bcrypt')
 const User = require('../schemas/UserSchema')
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// router.get("/", (req, res, next) => {
-//     res.status(200).render("register");
-// })
 
 router.post("/", async (req, res, next) => {
 
